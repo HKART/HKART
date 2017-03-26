1efaa7bce531de4d07ee9ee64d4c4eaf849deb8c
susobhan <susobhan@acalvio.com>
2017-03-26 14:51:32 +0000

    minor changes in header file

--- hkart_cardiac.h
+++ hkart_cardiac.h
@@ -2,9 +2,8 @@
 #define HKART_CARDIAC_H
 
 typedef struct cardiac_panel_s {
-    float Apolipoprotein_A1;
-    float Apolipoprotein_B;
-    float Homocysteine;
+    float Apolipoprotein_A1; //120 mg/dL
+    float Apolipoprotein_B; //40-125 mg/dL
     float Lipoprotein_A;
     float S_Homocysteine;
     float S_Fibrinogen;
@@ -15,8 +14,6 @@ typedef struct cardiac_panel_ref_s {
     float Apolipoprotein_A1_max;
     float Apolipoprotein_B_min;
     float Apolipoprotein_B_max;
-    float Homocysteine_min;
-    float Homocysteine_max;
     float Lipoprotein_A_min;
     float Lipoprotein_A_max;
     float S_Homocysteine_min;
