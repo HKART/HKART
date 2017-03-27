d933702814295e79258c9552a1be53f011b4a0db
root <root@localhost.localdomain>
2017-03-27 05:46:09 -0400

    modify script display

--- script_cardiac.sh
+++ script_cardiac.sh
@@ -2,7 +2,7 @@
 
 echo "set terminal png
 set output '$1'
-set terminal png size 1000,1000
+set terminal png font arial 25 size 1600,1200
 plot 'tmp1' using 1 with linespoints title 'Apolipoprotein_A1', \
      'tmp2' using 1 with linespoints title 'Apolipoprotein_B', \
      'tmp3' using 1 with linespoints title 'Lipoprotein_A', \
